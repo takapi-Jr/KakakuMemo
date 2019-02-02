a72f2e2ef7e7d30479d7ceb2c994e4f502558919
takapi-Jr <tsoda@hotmail.co.jp>
2019-02-02 18:36:56 +0900

    MultiTriggerConverterクラスをConvertersフォルダへ移動。

--- KakakuMemo/KakakuMemo/KakakuMemo/ViewModels/AddProductPageViewModel.cs
+++ KakakuMemo/KakakuMemo/KakakuMemo/ViewModels/AddProductPageViewModel.cs
@@ -286,25 +286,4 @@ namespace KakakuMemo.ViewModels
             }
         }
     }
-
-    /// <summary>
-    /// MultiTrigger使用時の文字列の長さ比較用クラス
-    /// </summary>
-    public class MultiTriggerConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType,
-            object parameter, CultureInfo culture)
-        {
-            if ((int)value > 0)         // length > 0 ?
-                return true;            // some data has been entered
-            else
-                return false;           // input is empty
-        }
-
-        public object ConvertBack(object value, Type targetType,
-            object parameter, CultureInfo culture)
-        {
-            throw new NotSupportedException();
-        }
-    }
 }
